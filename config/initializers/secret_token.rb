3fd7531bc13a3c2ee83a249edfd82f33ca658bd8
Ryan Lane <ryan@ryanalane.com>
2011-10-13 17:54:58 -0700

    Initial commit (take 2).
    Starting over with a fresh configuration. Round 2! (or 3)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blp::Application.config.secret_token = 'ea2d70a42506344c08133f0d1dbf2ee3d6bb2d577973adbc64aadc9ad295c8cb2ae7a36b1b0cef498fb33d58bb6245370e8c50fb86b3daba54e1433e0ea296b1'
